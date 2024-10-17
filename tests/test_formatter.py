d2a795cabc2a494b434b3f613d9c4c5db0e0e6c4
Maruf <hossain.maruf186@gmail.com>
2024-10-16 21:47:15 -0400

    updated test files

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,38 +1,18 @@
 import unittest
-from quaeso import formatter
+from onurodh import formatter  # Updated import
 
 
 class TestFormatter(unittest.TestCase):
-    def test_format_metadata(self):
-        metadata = '{"const1": "const2"}'
-        formatted = formatter.format_metadata(metadata)
-        self.assertEqual(formatted, 'const1: const2')
-
-    def test_format_json_string(self):
-        json_string = '{"const1": "const2"}'
-        formatted = formatter.format_json_string(json_string, indent=3)
-        self.assertEqual(formatted, (
-            '{\n'
-            '   "const1": "const2"\n'
-            '}'
-        ))
-
-    def test_format_xml_string(self):
-        xml_string = '<const1><const2>const3</const2></const1>'
-        formatted = formatter.format_xml_string(xml_string, indent=3)
-        self.assertEqual(formatted, (
-            '<const1>\n'
-            '   <const2>const3</const2>\n'
-            '</const1>'
-        ))
-
-    def test_format_html_string(self):
-        html_string = '<html><body><p>const1</p></body></html>'
-        formatted = formatter.format_html_string(html_string, indent=3)
-        self.assertEqual(formatted, (
-            '<html>\n'
-            '   <body>\n'
-            '      <p>const1</p>\n'
-            '   </body>\n'
-            '</html>'
-        ))
+    def test_format_output(self):
+        response = {
+            'status_code': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'text': '{"message": "success"}'
+        }
+        formatted = formatter.format_output(response)
+        expected_output = (
+            "Status: 200\n"
+            "Headers: {'Content-Type': 'application/json'}\n"
+            "Body: {\"message\": \"success\"}\n"
+        )
+        self.assertEqual(formatted, expected_output)
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,36 +2,32 @@ import json
 import yaml
 import tempfile
 import unittest
-
-from quaeso import reader
+from onurodh import reader  # Updated import
 
 
 class TestReader(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.file_data = {"const1": "const2"}
+        cls.file_data = {"method": "GET", "url": "http://example.com"}
 
     def test_read_request_file_with_json_file(self):
         _, fpath = tempfile.mkstemp(suffix=".json")
         with open(fpath, "w") as f:
             json.dump(self.file_data, f)
-        self.assertEqual(reader.read_request_file(fpath), self.file_data)
+        self.assertEqual(reader.read_file(fpath), self.file_data)
 
     def test_read_request_file_with_yml_file(self):
         _, fpath = tempfile.mkstemp(suffix=".yml")
         with open(fpath, "w") as f:
             yaml.dump(self.file_data, f)
-        self.assertEqual(reader.read_request_file(fpath), self.file_data)
+        self.assertEqual(reader.read_file(fpath), self.file_data)
 
     def test_read_request_file_with_yaml_file(self):
         _, fpath = tempfile.mkstemp(suffix=".yaml")
         with open(fpath, "w") as f:
             yaml.dump(self.file_data, f)
-        self.assertEqual(reader.read_request_file(fpath), self.file_data)
+        self.assertEqual(reader.read_file(fpath), self.file_data)
 
-    def test_read_request_file_with_invalid_file(self):
-        _, fpath = tempfile.mkstemp(suffix=".asdf")
-        with open(fpath, "w") as f:
-            yaml.dump(self.file_data, f)
-        with self.assertRaises(reader.UnsupportedFileTypeException):
-            reader.read_request_file(fpath)
+    def test_invalid_file_extension(self):
+        with self.assertRaises(ValueError):
+            reader.read_file("invalid.txt")
